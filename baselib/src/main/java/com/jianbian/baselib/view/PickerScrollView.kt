28bc03d8a6831524aabb6e59ce69eccc52113720
Ajin <1034693671@qq.com>
2020-08-31 16:38:32 +0800

    PickerView修改

--- baselib/src/main/java/com/jianbian/baselib/view/PickerScrollView.kt
+++ baselib/src/main/java/com/jianbian/baselib/view/PickerScrollView.kt
@@ -4,72 +4,73 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.os.Handler
+import android.os.Looper
 import android.os.Message
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import com.jianbian.baselib.R
 import com.jianbian.baselib.mvp.impl.PickerListener
+import com.jianbian.baselib.utils.AppUtil
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.math.abs
+import kotlin.math.pow
 
 
 /**
  * 滚动选择器
  */
-abstract class PickerScrollView <T>(context: Context, attrs: AttributeSet): View(context,attrs) {
-
-    val MARGIN_ALPHA = 2.8f
-    val SPEED = 2f
-
+abstract class PickerScrollView <T>: View {
+    var listener: PickerListener<T>? = null
     private var mDataList: MutableList<T> = ArrayList<T>()
-
-    /**
-     * 选中的位置，这个位置是mDataList的中心位置，一直不变
-     */
-    private var mCurrentSelected = 0
-    private var mPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private var mMaxTextSize = 20f
-    private var mMinTextSize = 10f
     private val mMaxTextAlpha = 255f
     private val mMinTextAlpha = 120f
-    private val mColorText = 0x333333
+    private var mMaxTextSize = 20f
+    private var mMinTextSize = 10f
     private var mViewHeight = 0
     private var mViewWidth = 0
-    private var mLastDownY = 0f
+    private var isInit = false
+
+    private var selectedTextColor = 0
+    private var selectedNotTextColor = 0
+    private var loop:Boolean = false//是否循环
+    private var speed = 2f
+    val MARGIN_ALPHA = 2.8f
+
     /**
      * 滑动的距离
      */
+    private var mCurrentSelected = 0
+    private var mLastDownY = 0f
     private var mMoveLen = 0f
-    private var isInit = false
-    private var mSelectListener: PickerListener<T>? = null
-    private var timer: Timer = Timer()
-    private var mTask: MyTimerTask? = null
-    var updateHandler: Handler = object : Handler() {
+    private var updateHandler: Handler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
-            if (abs(mMoveLen) < SPEED) {
-                mMoveLen = 0f
-                if (mTask != null) {
-                    mTask?.cancel()
-                    mTask = null
-                    performSelect()
-                }
-            } else  // 这里mMoveLen / Math.abs(mMoveLen)是为了保有mMoveLen的正负号，以实现上滚或下滚
-                mMoveLen -= mMoveLen / Math.abs(mMoveLen) * SPEED
+            mMoveLen = 0F
+            if (0 <mCurrentSelected &&mCurrentSelected < mDataList.size){
+                listener?.onSelect(mDataList[mCurrentSelected])
+            }
             invalidate()
         }
     }
-
-
-    init {
-        mDataList = ArrayList()
-        mPaint.style = Paint.Style.FILL
-        mPaint.textAlign = Paint.Align.CENTER
-        mPaint.color = mColorText
+    constructor(context: Context) : super(context) {
+        initView(null)
+    }
+    constructor(context: Context,attributeSet: AttributeSet?) : super(context,attributeSet) {
+        initView(attributeSet)
+    }
+    constructor(context: Context,attributeSet: AttributeSet?, defStyleAttr: Int) : super(context,attributeSet,defStyleAttr) {
+        initView(attributeSet)
     }
 
-    private fun performSelect() {
-        mSelectListener?.onSelect(mDataList[mCurrentSelected])
+    private fun initView(attrs: AttributeSet?){
+        if (attrs == null)return
+        val array = context.obtainStyledAttributes(attrs, R.styleable.PickerScrollView)
+        selectedTextColor = array.getColor(R.styleable.PickerScrollView_aj_pick_select_color,AppUtil.getColor(context,R.color.gray_3333))
+        selectedNotTextColor = array.getColor(R.styleable.PickerScrollView_aj_pick_select_not_color,AppUtil.getColor(context,R.color.gray_3333))
+        loop = array.getBoolean(R.styleable.PickerScrollView_aj_pick_loop,false)
+        speed = array.getFloat(R.styleable.PickerScrollView_aj_pick_speed,2F)
     }
 
     fun setData(datas: ArrayList<T>) {
@@ -86,20 +87,8 @@ abstract class PickerScrollView <T>(context: Context, attrs: AttributeSet): View
      *
      * @param selected
      */
-    public fun setSelected(selected: Int) {
+    fun setSelected(selected: Int) {
         mCurrentSelected = selected
-        val distance = mDataList!!.size / 2 - mCurrentSelected
-        if (distance < 0) {
-            for (i in 0 until -distance) {
-                moveHeadToTail()
-                mCurrentSelected--
-            }
-        } else if (distance > 0) {
-            for (i in 0 until distance) {
-                moveTailToHead()
-                mCurrentSelected++
-            }
-        }
         invalidate()
     }
 
@@ -117,28 +106,6 @@ abstract class PickerScrollView <T>(context: Context, attrs: AttributeSet): View
         }
     }
 
-    /**
-     * 数据将最后一个移动到最后一个
-     */
-    private fun moveHeadToTail() {
-        if (!mDataList.isNullOrEmpty()){
-            val datasBean:T = mDataList[0]
-            mDataList.removeAt(0)
-            mDataList.add(datasBean)
-        }
-    }
-
-    /**
-     * 数据讲最后一个移动到第一个
-     */
-    private fun moveTailToHead() {
-        if (!mDataList.isNullOrEmpty()){
-            val datasBean:T = mDataList[mDataList.size - 1]
-            mDataList.removeAt(mDataList.size - 1)
-            mDataList.add(0, datasBean)
-        }
-    }
-
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         mViewHeight = measuredHeight
@@ -152,17 +119,9 @@ abstract class PickerScrollView <T>(context: Context, attrs: AttributeSet): View
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         if (isInit){
-            val scale = parabola(mViewHeight / 4.0f, mMoveLen)
-            val size = (mMaxTextSize - mMinTextSize) * scale + mMinTextSize
-            mPaint.textSize = size
-            mPaint.alpha = ((mMaxTextAlpha - mMinTextAlpha) * scale + mMinTextAlpha).toInt()
-            val x = (mViewWidth / 2.0).toFloat()
-            val y = (mViewHeight / 2.0 + mMoveLen).toFloat()
-            val fmi: Paint.FontMetricsInt = mPaint.fontMetricsInt
-            val baseline = (y - (fmi.bottom / 2.0 + fmi.top / 2.0)) as Float
-            val textData: String = drawText(mDataList[mCurrentSelected])
-            canvas.drawText(textData, x, baseline, mPaint)
-
+            if (0 <mCurrentSelected &&mCurrentSelected < mDataList.size){
+                drawText(mCurrentSelected,canvas)
+            }
             for (index in 1..mCurrentSelected){
                 drawOtherText(canvas, index, -1)
             }
@@ -172,23 +131,38 @@ abstract class PickerScrollView <T>(context: Context, attrs: AttributeSet): View
         }
     }
 
+    private fun  drawText(index:Int,canvas: Canvas){
+        var mPaint = getPaint(selectedTextColor)
+        mPaint.textSize = mMaxTextSize
+        mPaint.alpha = mMaxTextAlpha.toInt()
+        val textData: String = drawText(mDataList[index])
+        canvas.drawText(textData, (mViewWidth / 2.0).toFloat(),(mViewHeight / 2.0 + mMoveLen/2.0).toFloat(), mPaint)
+    }
+
     /**
      * @param canvas
      * @param position 距离mCurrentSelected的差值
      * @param type     1表示向下绘制，-1表示向上绘制
      */
     private fun drawOtherText(canvas: Canvas, position: Int, type: Int) {
+        var mPaint = getPaint(selectedNotTextColor)
         val d = (MARGIN_ALPHA * mMinTextSize * position + type * mMoveLen)
         val scale = parabola(mViewHeight / 4.0f, d)
         val size = (mMaxTextSize - mMinTextSize) * scale + mMinTextSize
         mPaint.textSize = size
         mPaint.alpha = ((mMaxTextAlpha - mMinTextAlpha) * scale + mMinTextAlpha).toInt()
         val y = (mViewHeight / 2.0 + type * d).toFloat()
-        val fmi: Paint.FontMetricsInt = mPaint.fontMetricsInt
-        val baseline = (y - (fmi.bottom / 2.0 + fmi.top / 2.0)) as Float
         val indexs = mCurrentSelected + type * position
         val textData: String = drawText(mDataList[indexs])
-         canvas.drawText(textData, (mViewWidth / 2.0).toFloat(), baseline, mPaint)
+        canvas.drawText(textData, (mViewWidth / 2.0).toFloat(),y, mPaint)
+    }
+
+    private fun getPaint(mColorText: Int):Paint{
+        var mPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        mPaint.style = Paint.Style.FILL
+        mPaint.textAlign = Paint.Align.CENTER
+        mPaint.color = mColorText
+        return mPaint
     }
 
     /**
@@ -199,68 +173,54 @@ abstract class PickerScrollView <T>(context: Context, attrs: AttributeSet): View
      * @return scale
      */
     private fun parabola(zero: Float, x: Float): Float {
-        val f = (1 - Math.pow(x / zero.toDouble(), 2.0)).toFloat()
+        val f = (1 - (x / zero.toDouble()).pow(2.0)).toFloat()
         return if (f < 0) 0F else f
     }
 
     /*----------------------移动---------------------------------------*/
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.actionMasked) {
-            MotionEvent.ACTION_DOWN -> doDown(event)
-            MotionEvent.ACTION_MOVE -> doMove(event)
-            MotionEvent.ACTION_UP -> doUp(event)
+            MotionEvent.ACTION_DOWN -> {
+                updateHandler.removeCallbacksAndMessages(null)
+                mLastDownY = event.y
+            }
+            MotionEvent.ACTION_MOVE -> {
+                mMoveLen += event.y - mLastDownY
+                actionMove()
+                mLastDownY = event.y
+                invalidate()
+            }
+            MotionEvent.ACTION_UP ,MotionEvent.ACTION_CANCEL-> {
+                updateHandler.removeCallbacksAndMessages(null)
+                updateHandler.sendEmptyMessageDelayed(1,10)
+            }
         }
         return true
     }
 
-    private fun doDown(event: MotionEvent) {
-        if (mTask != null) {
-            mTask?.cancel()
-            mTask = null
-        }
-        mLastDownY = event.y
-    }
-
-    private fun doMove(event: MotionEvent) {
-        mMoveLen += event.y - mLastDownY
+    private fun actionMove(){
         if (mMoveLen > MARGIN_ALPHA * mMinTextSize / 2) {
-            // 往下滑超过离开距离
-            moveTailToHead()
             mMoveLen -= MARGIN_ALPHA * mMinTextSize
+            mCurrentSelected--
         } else if (mMoveLen < -MARGIN_ALPHA * mMinTextSize / 2) {
-            // 往上滑超过离开距离
-            moveHeadToTail()
             mMoveLen += MARGIN_ALPHA * mMinTextSize
+            mCurrentSelected++
         }
-        mLastDownY = event.y
-        invalidate()
-    }
-
-    private fun doUp(event: MotionEvent) {
-        // 抬起手后mCurrentSelected的位置由当前位置move到中间选中位置
-        if (abs(mMoveLen) < 0.0001) {
-            mMoveLen = 0f
-            return
-        }
-        if (mTask != null) {
-            mTask?.cancel()
-            mTask = null
-        }
-        mTask = MyTimerTask(updateHandler)
-        timer.schedule(mTask, 0, 10)
-    }
-    /*----------------------移动完成---------------------------------------*/
-
-    internal inner class MyTimerTask(handler: Handler) : TimerTask() {
-        var handler: Handler
-        override fun run() {
-            handler.sendMessage(handler.obtainMessage())
-        }
-
-        init {
-            this.handler = handler
+        if (mCurrentSelected <= 0) {
+            mCurrentSelected = 0
+            mMoveLen = -mMoveLen
+        } else if (mCurrentSelected >= mDataList.size) {
+            mCurrentSelected = mDataList.size - 1
+            mMoveLen = -mMoveLen
         }
     }
 
     abstract fun drawText(data:T) : String
+
+    fun getSelectItem():T?{
+        if (0 <mCurrentSelected &&mCurrentSelected < mDataList.size){
+            return mDataList[mCurrentSelected]
+        }
+        return null
+    }
 }
