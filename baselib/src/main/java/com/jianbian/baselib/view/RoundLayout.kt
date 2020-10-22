59e63d6e8e996e50bd6ce48a59baa9e7cb42a056
Ajin <1034693671@qq.com>
2020-10-22 22:10:55 +0800

    修复RoundLayout 适配问题

--- baselib/src/main/java/com/jianbian/baselib/view/RoundLayout.kt
+++ baselib/src/main/java/com/jianbian/baselib/view/RoundLayout.kt
@@ -4,27 +4,28 @@ import android.content.Context
 import android.graphics.*
 import android.os.Build
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.widget.FrameLayout
 import com.jianbian.baselib.R
 
 class RoundLayout (context: Context,attrs: AttributeSet):FrameLayout(context,attrs){
     private var redis = 0
-    private var path: Path? = null
+    private var intercept = false
     init {
         val array = context.obtainStyledAttributes(attrs, R.styleable.RoundLayout)
         redis = array.getDimensionPixelSize(R.styleable.RoundLayout_aj_round_layout_radius, 0)
+        intercept = array.getBoolean(R.styleable.RoundLayout_aj_intercept,false)
+    }
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        return intercept
     }
 
     override fun dispatchDraw(canvas: Canvas) {
-        if (path == null) path = Path()
+        val path = Path()
         path?.reset()
         path?.addRoundRect(RectF(0F, 0F, measuredWidth.toFloat(), measuredHeight.toFloat()), redis.toFloat(), redis.toFloat(), Path.Direction.CW)
         canvas.drawFilter = PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
-        if (Build.VERSION.SDK_INT >= 28) {
-            canvas.clipPath(path!!)
-        } else {
-            canvas.clipPath(path!!, Region.Op.REPLACE)
-        }
+        canvas.clipPath(path!!)
         super.dispatchDraw(canvas)
     }
 }
