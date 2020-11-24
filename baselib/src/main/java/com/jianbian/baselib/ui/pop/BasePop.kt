3c1bd2d029971d4d721fff7cc2edd088ed1afb16
Ajin <1034693671@qq.com>
2020-11-24 17:33:19 +0800

    baseAdapter修改

--- baselib/src/main/java/com/jianbian/baselib/ui/pop/BasePop.kt
+++ baselib/src/main/java/com/jianbian/baselib/ui/pop/BasePop.kt
@@ -1,8 +1,11 @@
 package com.jianbian.baselib.ui.pop
 
 import android.content.Context
+import android.graphics.Rect
 import android.graphics.drawable.ColorDrawable
+import android.os.Build
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupWindow
 import androidx.annotation.LayoutRes
@@ -23,4 +26,13 @@ abstract class BasePop(context: Context) : PopupWindow(context) {
     @LayoutRes
     abstract fun LayoutRes(): Int
     abstract fun initView()
+
+    fun changeHeight(anchor:View?){
+        if (Build.VERSION.SDK_INT >= 24 && anchor != null) {
+            val rect = Rect()
+            anchor.getGlobalVisibleRect(rect)
+            val h: Int = anchor.resources.displayMetrics.heightPixels - rect.bottom
+            height = h
+        }
+    }
 }
