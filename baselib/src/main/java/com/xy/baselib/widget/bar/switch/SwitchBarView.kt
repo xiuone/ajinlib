6eff713e566160acfca7dc63791463599dd4a447
yujingang <2994029340@qq.com>
2022-05-05 09:32:34 +0800

    添加switchBar的样式

--- baselib/src/main/java/com/xy/baselib/widget/bar/switch/SwitchBarView.kt
+++ baselib/src/main/java/com/xy/baselib/widget/bar/switch/SwitchBarView.kt
@@ -15,9 +15,11 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.OnLifecycleEvent
 import com.xy.baselib.R
 import com.xy.utils.addAlpha
+import com.xy.utils.drawCenterText
 import com.xy.utils.getResColor
 import com.xy.utils.showToast
 import kotlin.math.abs
+import kotlin.math.max
 import kotlin.math.min
 
 class SwitchBarView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0)
@@ -32,10 +34,15 @@ class SwitchBarView @JvmOverloads constructor(context: Context, attrs: Attribute
     private var thumbOffColor = defaultWhiteColor
     private var thumbOpenColor = defaultBlueColor
     //是否绘制文字
-    private var drawText = false;
+    private var isDrawText = false;
 
-    private var thumbOffText = "";
-    private var thumbOpenText = "";
+    private var thumbOffText = ""
+    private var thumbOffTextColor = defaultWhiteColor
+    private var thumbOffTextSize = 15F
+
+    private var thumbOpenText = ""
+    private var thumbOpenTextColor = defaultWhiteColor
+    private var thumbOpenTextSize = 15F
 
     private var thumbShadowColor : Int = defaultGraColor
     private var thumbShadowRadius : Float = 0F
@@ -51,9 +58,16 @@ class SwitchBarView @JvmOverloads constructor(context: Context, attrs: Attribute
             mSelectBackgroundColor = typedArray.getColor(R.styleable.SwitchBarView_switch_select_background_color, defaultBlueColor)
             thumbOffColor = typedArray.getColor(R.styleable.SwitchBarView_switch_thumb_off_color, defaultWhiteColor)
             thumbOpenColor = typedArray.getColor(R.styleable.SwitchBarView_switch_thumb_open_color, defaultWhiteColor)
-            drawText = typedArray.getBoolean(R.styleable.SwitchBarView_switch_draw_text, false)
+            isDrawText = typedArray.getBoolean(R.styleable.SwitchBarView_switch_draw_text, false)
+
             thumbOffText = typedArray.getString(R.styleable.SwitchBarView_switch_thumb_off_text)?:""
+            thumbOffTextSize = typedArray.getDimension(R.styleable.SwitchBarView_switch_thumb_off_text_size,thumbOpenTextSize)
+            thumbOffTextColor = typedArray.getColor(R.styleable.SwitchBarView_switch_thumb_off_text_color,thumbOpenTextColor)
+
             thumbOpenText = typedArray.getString(R.styleable.SwitchBarView_switch_thumb_open_text)?:""
+            thumbOpenTextSize = typedArray.getDimension(R.styleable.SwitchBarView_switch_thumb_open_text_size,thumbOffTextSize)
+            thumbOpenTextColor = typedArray.getColor(R.styleable.SwitchBarView_switch_thumb_open_text_color,thumbOpenTextColor)
+
             thumbShadowColor = typedArray.getColor(R.styleable.SwitchBarView_switch_thumb_shadow_color, defaultGraColor)
             thumbShadowRadius = typedArray.getDimension(R.styleable.SwitchBarView_switch_thumb_shadow_radius, thumbShadowRadius)
             isSwitchSelect = typedArray.getBoolean(R.styleable.SwitchBarView_switch_is_select, false)
@@ -156,7 +170,34 @@ class SwitchBarView @JvmOverloads constructor(context: Context, attrs: Attribute
         super.onDraw(canvas)
         val canvas = canvas?:return
         drawBackground(canvas)
+        drawText(canvas)
         drawThumb(canvas)
+    }
+
+    /**
+     * 绘制文字
+     */
+    private fun drawText(canvas: Canvas){
+        if (!isDrawText)return
+        val  offPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        offPaint.color = thumbOffTextColor
+        offPaint.textSize = thumbOffTextSize
+
+        val  openPaint = Paint(Paint.ANTI_ALIAS_FLAG)
+        openPaint.color = thumbOpenTextColor
+        openPaint.textSize = thumbOpenTextSize
+
+        if (!thumbOffText.isNullOrEmpty()){
+            val center = min(height/2,width/2).toFloat()
+            canvas.drawCenterText(center,center,thumbOffText,offPaint)
+        }
+
+        if (!thumbOpenText.isNullOrEmpty()){
+            val center = min(height/2,width/2).toFloat()
+            val centerX = if (isH()) (width - center) else center
+            val centerY = if (!isH()) (height - center) else center
+            canvas.drawCenterText(centerX,centerY,thumbOpenText,openPaint)
+        }
     }
 
     /**
