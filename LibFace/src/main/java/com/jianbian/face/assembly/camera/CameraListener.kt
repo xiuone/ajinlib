926d5231a6056927e5f9bcefdc8b1eb7227af283
yujingang <2994029340@qq.com>
2023-06-07 23:20:47 +0800

    IssueID= Description=

--- LibFace/src/main/java/com/jianbian/face/assembly/camera/CameraListener.kt
+++
@@ -1,9 +0,0 @@
-package com.jianbian.face.assembly.camera
-
-import com.jianbian.face.view.CameraSurfaceView
-
-interface CameraListener {
-    fun startPreview(surfaceView: CameraSurfaceView)
-    fun stopPreview()
-    fun onPreviewFrame(surfaceView: CameraSurfaceView,data: ByteArray?) {}
-}
--- LibLoginOne/src/main/java/com/xy/libonelogin/LoginOneKeyListener.kt
+++
@@ -1,5 +0,0 @@
-package com.xy.libonelogin
-
-interface LoginOneKeyListener {
-    fun loginOneKey(token:String?)
-}
